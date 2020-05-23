6f84f0bf95fb11b7d3f9a72dc521a8dcf26bcb43
lucasaac <lalonso@finkok.com.mx>
2020-05-22 23:15:40 -0500

    se agrego el localstorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Formulario from './componentes/Formulario'; 
 import Cita from './componentes/Cita'; 
 
 
 function App() {
 
+  //Citas en localstorage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales) citasIniciales = []; 
+
   //Arreglo de citas
-  const[citas, guardarCitas] = useState([]);
+  const[citas, guardarCitas] = useState(citasIniciales);
+
+  //Use Effect para realizar ciertas operaciones cuando el state cambia.
+  useEffect(()=>{
+
+    if (citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(citas))
+    }else{
+      localStorage.setItem('citas', JSON.stringify([]))
+    }
+
+  }, [citas]); // EL [] => es un Array de Dependencias.
+
 
   //Funcion que tome las citas actuales y agrege la nueva
   const crearCita = cita => {
